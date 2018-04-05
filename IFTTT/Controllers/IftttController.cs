f48ff5bb707c2c1358f614df3cf006dbaa530b14
Matúš Bačkor <matus.backor@gmail.com>
2018-04-05 19:13:38 +0200

    Return 503 when key is invalid

--- IFTTT/Controllers/IftttController.cs
+++ IFTTT/Controllers/IftttController.cs
@@ -10,14 +10,32 @@ namespace IFTTT.Controllers
 {
     public class IftttController : ApiController
     {
+        const string KEY = "cDxBDvAD_g-2azRWdnKqlqBcRptNxeL2l3LKw8R2TJcpcyW7RSNZaPmRx-o_9R7k";
+
         // GET: api/Ifttt
         [Route("ifttt/v1/status")]
         public async Task<IHttpActionResult> GetAsync()
-            => await Task.FromResult(StatusCode(HttpStatusCode.OK));
+        {
+            string key = "";
+            IEnumerable<string> values;
+            if (Request.Headers.TryGetValues("IFTTT-Channel-Key", out values))
+            {
+                key = values.FirstOrDefault();
+            }
+
+            if (key.Equals(KEY))
+            {
+                return await Task.FromResult(StatusCode(HttpStatusCode.OK));
+            }
+
+            return await Task.FromResult(StatusCode(HttpStatusCode.ServiceUnavailable));
+        }
 
         // GET: api/Ifttt/5
         public string Get(int id)
         {
+            IEnumerable<string> values;
+            Request.Headers.TryGetValues("IFTTT-Channel-Key", out values);
             return "value";
         }
 
