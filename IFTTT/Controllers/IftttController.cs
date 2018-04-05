e54dba3b320d473f8a49de198323325d83c24d40
Matúš Bačkor <matus.backor@gmail.com>
2018-04-05 18:49:37 +0200

    Add status endpoint return 200

---
+++ IFTTT/Controllers/IftttController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace IFTTT.Controllers
+{
+    public class IftttController : ApiController
+    {
+        // GET: api/Ifttt
+        [Route("ifttt/v1/status")]
+        public async Task<IHttpActionResult> GetAsync()
+            => await Task.FromResult(StatusCode(HttpStatusCode.OK));
+
+        // GET: api/Ifttt/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Ifttt
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Ifttt/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Ifttt/5
+        public void Delete(int id)
+        {
+        }
+
+        public async Task GetTask()
+            => await Task.CompletedTask;
+    }
+}
